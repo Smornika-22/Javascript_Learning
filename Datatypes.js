63bcefbdd9988ea7a2c971cdfb7db721c7d175de
Smornika-22 <smornikamohanty2002@gmail.com>
2025-01-25 04:33:38 +0000

    Datatype Done

---
+++ Datatypes.js
@@ -0,0 +1,13 @@
+const accountId = 136738  //For constant declaration
+let accountEmail = "Smornika@gmail.com" //For Varaiable declaration
+accountCity = "Jaipur" //For Varaiable declaration
+var accountPassword = "235536"
+//var does not understand the scope of variable(Block scope and functional scope) it creates a problem so we generally use const and let
+//accountId = 2 //Constant variable so not be changed
+/*multi line comment*/
+let accountState;
+accountEmail = "adafa"
+accountCity = "Kolhapur"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
